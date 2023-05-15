72bc4f32bb6fd4ddc249fc1a9ddff3dd0195f3c0
badbad <703735185@qq.com>
2023-05-15 16:34:31 +0800

    commit src file

---
+++ src/test/java/com/dailycodebuffe/cloud/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffe.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
